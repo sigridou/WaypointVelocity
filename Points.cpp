c9a0f5115b7f3b6e2b9fa229c2fac954c6c981fe
yaslou <101998818+sigridou@users.noreply.github.com>
2022-06-09 21:01:44 +0100

    Add files via upload

--- Points.cpp
+++ Points.cpp
@@ -1,3 +1,4 @@
+
 // Sss.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include "ros/ros.h"
@@ -6,33 +7,58 @@
  #include <ros/console.h>
 #include <iostream>
 
-#include <iostream>
+
 #include <vector>
 #define PI 3.14159265
 #include <math.h>
+#include <stdlib.h>
+#include <ctype.h>  
+#define PI 3.14159265
 
 
+struct Point{
+  float x[100];
+  float y[100];
+  float theta[100];
 
-class Point {
-public:
-	double x, y, z, vl, va;
-
-	Point(double x, double y, double z, double vl, double va) {
-		this->x = x;
-		this->y = y;
-		this->z = z;
-		this->vl = vl;
-		this->va = va;
-
-	};
 };
 
 
+char * strtoupper( char * dest, const char * src ) {
+    char * result = dest;
+    while( *dest++ = toupper( *src++ ) );
+    return result;
+}
 
+// fonction 1
+void generPoint(int d, float v ,float dt,  float omega )
+{   Point pInit ;
 
-int main(int argc, char* argv[]) {
+   pInit.x[0] = 0;
+    pInit.y [0]= 0;
+    pInit.theta[0] = 0*(PI/180) ;
+		 for(int h=0 ;h<((d/v)/dt);h++){
+		 
+	    pInit.x[h+1]=pInit.x[h]-v*dt* sin(pInit.theta[h]+(dt*omega)/2);
+	  	pInit.y[h+1]=pInit.y[h]+v*dt* cos(pInit.theta[h]+(dt*omega)/2);
+	  	pInit.theta[h+1]=pInit.theta[h]+dt*omega;
+	 	
+		printf("Position du point: x:  %f\t y: %f\t z:  %f\t  \n" ,pInit.x[h+1],pInit.y[h+1],pInit.theta[h+1]);
+	  
+	  
+		}
+	
+}
 
+// fonction 2
+//float move_forward(float deltafi,int q )
+//{  
+ //float omegaa = deltafi*q ;
+//return (omegaa);
+//}
+ 
 
+int main(int argc, char** argv) {
 
 ros::init(argc, argv, "Points");
         ros::NodeHandle nd;
@@ -42,103 +68,63 @@ ros::init(argc, argv, "Points");
 
 {       loop_rate.sleep();
 
-	int np, nh, j, d;
-	double  t;
-	double xf, yf;
-	double zf;
-	double cx=0;
-	double cz=45*PI/180;
-	double cy=0;
-	np = 3;
+
+	const float pi =  3.14159265358979323846;
+	struct Point pInit ; //point, pointFinal;
+	int n,d,q,i,k,j,a;   //le nombre de trajectoire
 	
-	d = 2;
+     d=1;    // la distance en metre 
+    float deltafi_av,deltafi_dg,omega,omegaa, dt,v; // les parametres
+  
 
-	Point p(0, 0, 0, 0, 0);
-	Point v[4][1] = { {p},{p},{p},{p} };
+    char commande[20]  ;
+//initialisation des points (x,y,theta)
 	
-	double tmp = 2;
+   
+    printf("Coordonees du point initial sont x ,y ,theta : %f \t  %f \t   %f \n", pInit.x[0],pInit.y[0],pInit.theta[0]);
 
-	int i = 0;
-	j = 0;
+ // commande pour declarer le n ,dt,v
+ n=7;
+printf("Pour une trajectoire avec n=7 points on a :\n");
+printf("Vitesse lineaire = 0.2m/s et un temps de deplacement delta t = 2s\n");
+v=0.2;
+dt=2;
 
 
-	for (i = 0; i <=( np - 1); i++)
-	{
+//les zone definit pour les commande (radian)	 
+	 deltafi_av=(0.3)/n;  // pour la commande avant
+	 deltafi_dg=(0.4)/n;
+
+
+
+// la boucle de omega par rapport aux nmbrs de trajectoire 	 	 
+i=(-(n-1)/2);
+
+	 while  (i<=(n-1)/2) {
+	 
+
+	 omega= (((0.8/n)/dt)*i);   //omerga calculer avec deltaFi(l'angle entre les trajectoires)
 	
-
-       for(j=0;j<1;j++)
-		{
-                   ROS_INFO("usage: add_two_ints_client X Y");
-		  printf("Le point initial : Position x: %f , Position y: %f, L'angle: %f  test\n", cx, cy, cz);
+	
+	
+// pour chaque omega un nmbr h de point generer 
+   printf("\n");
+	printf(" Avec une vitesse angulaire Omega = %f :\n  ", omega);
 		
-
-
-		
-				t = (cz*i) / (np - 1);
-				
-				xf = cx + d * (cos((t + cz)));
-				yf = cy + d* (sin((t + cz)));
-				zf = cz   +t;
-				v[i][j].x = xf;
-				v[i][j].y = yf;
-				v[i][j].z = zf;
-				v[i][j].vl = 0;
-				v[i][j].va = 0;
-
-			
-
-
-
-
-			//X[j] = xf;
-			//Y[j] = yf;
-			printf("Position x: %f , Position y: %f, L'angle: %f", v[i][j].x, v[i][j].y, v[i][j].z);
-                
-			printf("\n");
-                        
-
-	}
-		printf("\n");
-	}
-	double disl, disa;
-	disl = 0;
-	disa=0;
-	printf("Resultat avec les vitesses\n");
-
-	for (i = 0; i <= np - 1; i++)
-	{
-		for (j = 0; j < 1; j++)
-		{
-				disl = sqrt(pow((v[i][j].x - cx), 2) + pow((v[i][j].y - cy), 2));
-		
-					v[i][j].vl = disl / tmp;
-				disa = (v[i][j].z - cz);
-				v[i][j].va = disa / tmp;
-			
-
-			printf("Position x: %f , Position y: %f, L'angle: %f,La vitesse Angulaire: %f, La vitesse Lineaire: %f", v[i][j].x, v[i][j].y, v[i][j].z, v[i][j].va, v[i][j].vl);
-			printf("\n");
-
-
-		}
-
-
-		printf("\n");
-
-	}
-
-
-
-
-
+	   generPoint( d,  v , dt,  omega );
+	
+	i++;
+	
+	   
+	  
+    
+	
+	 }
 ros::spinOnce();
 loop_rate.sleep();
 }
-  
 
-
-
-
+ 
 
 	return 0;
 }
